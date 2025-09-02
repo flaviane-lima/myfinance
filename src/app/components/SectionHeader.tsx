466db3659f5fb471c1065f8c933b68d021754ff7
flaviane-lima <rflaviane41@gmail.com>
2025-09-02 19:42:59 -0300

    Estiliza SectionHeader com media queries para melhorar visual em telas menores

--- src/app/components/SectionHeader.tsx
+++ src/app/components/SectionHeader.tsx
@@ -4,11 +4,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Title = styled.h2`
-  font-size:  1.125rem; // equivalente a 14px
+  font-size:  1.125rem; // equivalente a 18px
   font-weight: 400;
   margin-top: 0;
   margin-bottom: 0.75rem;
   color: #374151;  
+  text-align: center;
+
+  @media (max-width: 600px){
+     font-size: 1rem;
+  }
+  
+  @media (max-width: 400px){
+     font-size: 0.875rem;
+  }
 `;
 
 export default function SectionHeader({ children }: { children: React.ReactNode }){
