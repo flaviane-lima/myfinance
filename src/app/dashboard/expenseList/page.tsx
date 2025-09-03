21f92a57164f2bd493a3e1499c91a262ed759fb4
flaviane-lima <rflaviane41@gmail.com>
2025-09-03 18:08:59 -0300

    adicionar fundo na página para melhor contraste visual

--- src/app/dashboard/expenseList/page.tsx
+++ src/app/dashboard/expenseList/page.tsx
@@ -1,12 +1,20 @@
-
+'use client'
 
 import ExpenseList from "../../components/ExpenseList";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  background-color: #f9fafb; // ✅ Cinza bem clarinho
+  min-height: 100vh; // ✅ Garante que ocupa toda a altura da tela
+  padding: 1rem 0; // ✅ Adiciona um pouco de espaço vertical
+`
 
 export default function List() {
 
     return(
-        <div>
+        <PageContainer>
             <ExpenseList />
-        </div>
+        </PageContainer>    
+        
     )
 }
